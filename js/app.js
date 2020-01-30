44d174d3c41b854b88cb4dfc6da4a8e37a673b39
natalie sinner <nataliesinner@hotmail.com>
2020-01-29 21:14:33 -0800

    commit set up for question3

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var storeHoursArr = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+var storeHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
 var location1 = {
   name: 'Seattle',
@@ -12,5 +12,14 @@ var location1 = {
   }
 };
 
+for(var i = 0; i < storeHours.length; i++){
+  console.log(location.name, storeHours[i]);
+//   create var for multipling randomCust with avgCookieSale = number of cookies sold that hour
+// create li in HTML for each store with <ul id="hfjdabfhadsb">
+// create var for HTML element (li) = document.createElement('li')
+// HTML element.textContent = string that you want it to read (7am: 500 cookies) by concat 
+// append cookies sold that hour to the li
+  var 
+}
 console.log(location1.name);
 console.log(location1.randomCust());
