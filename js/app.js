6282d4a64836675807b509b12c18e5132fb2636c
natalie sinner <nataliesinner@hotmail.com>
2020-01-29 22:46:31 -0800

    added id to html

--- js/app.js
+++ js/app.js
@@ -14,12 +14,13 @@ var location1 = {
 
 for(var i = 0; i < storeHours.length; i++){
   console.log(location.name, storeHours[i]);
+  var hourlySoldCookies = location1.avgCookieSale * location1.randomCust;
+  console.log(hourlySoldCookies);
 //   create var for multipling randomCust with avgCookieSale = number of cookies sold that hour
 // create li in HTML for each store with <ul id="hfjdabfhadsb">
 // create var for HTML element (li) = document.createElement('li')
 // HTML element.textContent = string that you want it to read (7am: 500 cookies) by concat 
 // append cookies sold that hour to the li
-  var 
 }
 console.log(location1.name);
 console.log(location1.randomCust());
