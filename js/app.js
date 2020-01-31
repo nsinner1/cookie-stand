c89147c0a73752240c4603619f278e5ce5a29672
natalie sinner <nataliesinner@hotmail.com>
2020-01-30 20:04:04 -0800

    location2 complete, added total to html

--- js/app.js
+++ js/app.js
@@ -16,15 +16,15 @@ var location1 = {
     for(var i = 0; i < storeHours.length; i++){
       var hourlySoldCookies = (Math.round(this.avgCookieSale * Math.random() * (this.minHourlyCust, this.maxHourlyCust) + this.minHourlyCust));
       this.totalCookies += hourlySoldCookies;
-      console.log(this.totalCookies + 'total');
+      //   console.log(this.totalCookies + 'total');
 
       var listElement = document.createElement('li');
       listElement.textContent = `${storeHours[i]} ${hourlySoldCookies} cookies.`;
       seattleElement.appendChild(listElement);
-      console.log(storeHours[i]);
-      console.log(storeHours[i] + ' ' + hourlySoldCookies);
+    //   console.log(storeHours[i]);
+    //   console.log(storeHours[i] + ' ' + hourlySoldCookies);
     }
-    var totalElement = document.getElementById('Total');
+    var totalElement = document.getElementById('Total1');
     var listElement = document.createElement('li');
     listElement.textContent = `${totalSoldCookies} ${this.totalCookies} cookies.`;
     totalElement.appendChild(listElement);
@@ -34,26 +34,30 @@ location1.randomCust();
 
 
 // Object for second location
-// var location2 = {
-//   name: 'Tokyo',
-//   minHourlyCust: 3,
-//   maxHourlyCust: 24,
-//   avgCookieSale: 1.2,
-//   randomCust: function getRandomIntInclusive(){
-//     return Math.round(Math.random() * (this.minHourlyCust, this.maxHourlyCust) + this.minHourlyCust);
-//   }
-// };
-// console.log(location2.name);
-// console.log(location2.randomCust());
+var location2 = {
+  name: 'Tokyo',
+  minHourlyCust: 3,
+  maxHourlyCust: 24,
+  avgCookieSale: 1.2,
+  totalCookies: 0,
+  randomCust: function getRandomIntInclusive(){
+    var tokyoElement = document.getElementById('Tokyo');
+    for(var j = 0; j < storeHours.length; j++){
+      var hourlySoldCookies = (Math.round(this.avgCookieSale * Math.random() * (this.minHourlyCust, this.maxHourlyCust) + this.minHourlyCust));
+      this.totalCookies += hourlySoldCookies;
 
-// Loop for second location
-// for(var j = 0; j < storeHours.length; j++){
-//     console.log(location.name, storeHours[j]);
-//   var hourlySoldCookiesTokyo = location2.avgCookieSale * location2.randomCust;
-//   console.log(hourlySoldCookies);
-//   var listElement = document.createElement('li');
-//   listElement.textContent = `${storeHours} ${hourlySoldCookiesTokyo} cookies.`;
-// }
+      var listElement = document.createElement('li');
+      listElement.textContent = `${storeHours[j]} ${hourlySoldCookies} cookies.`;
+      tokyoElement.appendChild(listElement);
+    }
+    var totalElement = document.getElementById('Total2');
+    var listElement = document.createElement('li');
+    listElement.textContent = `${totalSoldCookies} ${this.totalCookies} cookies.`;
+    totalElement.appendChild(listElement);
+  }
+};
+location2.randomCust();
+
 
 // Object for third location
 // var location3 = {
